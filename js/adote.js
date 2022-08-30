cd721a2ccd6f677b31cc39fd5675325c62e39f25
Cinth010 <cinthia.rbs010@gmail.com>
2022-08-30 20:57:13 -0300

    corrigindo javascript adote.jse

--- js/adote.js
+++ js/adote.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const menu = document.querySelector('.menu')
 const NavMenu = document.querySelector('.nav-menu')
 
@@ -6,15 +5,3 @@ menu.addEventListener('click', () => {
   menu.classList.toggle('ativo')
   NavMenu.classList.toggle('ativo')
 })
-=======
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
-
-
-  
->>>>>>> e5ac45f62be1b323dcb9d89790889e1bae40257f
